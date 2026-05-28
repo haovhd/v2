ebb838c67b05fe46533377a09bfbbac914f0d582
haovhd <haovhd@gmail.com>
2026-05-28 23:51:41 +0800

    Add files via upload

---
+++ tvbox/zhan.php
@@ -0,0 +1,125 @@
+{
+  "sites": [
+    {
+      "key": "孟占朝AList免扫4K",
+      "name": "👖孟占朝丨Alist丨免扫4K",
+      "type": 3,
+      "jar": "https://gh-proxy.org/https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/refs/heads/Jsnzkpg/stymei/alist.jar",
+      "api": "csp_AList",
+      "searchable": 1,
+      "ext": "https://gh-proxy.org/https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/refs/heads/Jsnzkpg/stymei/alist.json"
+    },
+    {
+      "key": "孟占朝独播JS",
+      "name": "👖孟占朝丨独播丨JS",
+      "type": 3,
+      "api": "https://mpimg.cn/down.php/fbe5493683cbef26437721513d495d7d.js",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "孟占朝短剧JS",
+      "name": "👖孟占朝丨短剧丨JS",
+      "type": 3,
+      "api": "https://mpimg.cn/down.php/93e0decefe3a2ad8949c5fe389d6cab2.js",
+      "style": {
+        "type": "rect",
+        "ratio": 1.33
+      },
+      "changeable": 1
+    },
+    {
+      "key": "孟占朝央视PY",
+      "name": "👖孟占朝丨央视丨PY",
+      "type": 3,
+      "api": "https://mpimg.cn/down.php/2dfd46b7b542f1f4ac34e0af4ec5549b.py",
+      "searchable": 1,
+      "changeable": 1,
+      "quickSearch": 1,
+      "filterable": 1,
+      "playerType": 2
+    },
+    {
+      "key": "孟占朝栏目PY",
+      "name": "👖孟占朝丨栏目丨PY",
+      "type": 3,
+      "api": "https://mpimg.cn/down.php/83d7645e7fa012dba942c467774e7b2b.py",
+      "searchable": 1,
+      "changeable": 1,
+      "quickSearch": 1,
+      "filterable": 1,
+      "playerType": 2
+    },
+    {
+      "key": "孟占朝剧王短剧",
+      "name": "👖孟占朝丨剧王丨短剧",
+      "type": 3,
+      "api": "https://mpimg.cn/down.php/b9c6c381312dcd5b510ac3e6bb49aa0f.py",
+      "searchable": 1,
+      "changeable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "wsycj",
+      "name": "👖孟占朝丨4K丨采集",
+      "type": 1,
+      "api": "https://api.wsyzy.net/api.php/provide/vod",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1,
+      "categories": [
+        "4K电影", "Netflix电影", "Netflix自制剧", "动作片", "喜剧片", "爱情片", "科幻片", "恐怖片", "剧情片", "战争片", "纪录片", "动画片", "国产剧", "欧美剧", "韩剧", "日剧", "港剧", "台剧", "泰剧", "海外剧", "大陆综艺", "国产动漫", "日韩动漫", "欧美动漫", "反转爽剧", "古装仙侠", "现代都市", "短剧"
+      ]
+    },
+    {
+      "key": "ryzycj",
+      "name": "👖孟占朝丨如意丨采集",
+      "type": 1,
+      "api": "https://cj.rycjapi.com/api.php/provide/vod",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1,
+      "categories": [
+        "动作片", "灾难片", "爱情片", "科幻片", "战争片", "喜剧片", "恐怖片", "剧情片", "纪录片", "动画电影", "国产剧", "欧美剧", "香港剧", "韩国剧", "台湾剧", "日本剧", "海外剧", "泰国剧", "短剧", "大陆综艺", "日韩综艺", "国产动漫", "日韩动漫", "欧美动漫"
+      ]
+    },
+    {
+      "key": "dyttcj",
+      "name": "👖孟占朝丨天堂丨采集",
+      "type": 1,
+      "api": "http://dyttzyw.com/api.php/provide/vod/?ac=list",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1,
+      "categories": [
+        "动作片", "科幻片", "爱情片", "战争片", "动画片", "喜剧片", "恐怖片", "剧情片", "纪录片", "国产剧", "香港剧", "台湾剧", "海外剧", "短剧", "欧美剧", "韩国剧", "日本剧", "国产动漫", "欧美动漫", "日韩动漫", "大陆综艺"
+      ]
+    },
+    {
+      "key": "bfzycj",
+      "name": "👖孟占朝丨暴风丨采集",
+      "type": 1,
+      "api": "http://by.bfzyapi.com/api.php/provide/vod",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1,
+      "categories": [
+        "纪录片", "战争片", "剧情片", "爱情片", "科幻片", "恐怖片", "喜剧片", "动作片", "动画片", "现代言情", "反转爽文", "闪婚离婚", "古装仙侠", "电影解说", "大陆综艺", "日韩动漫", "欧美动漫", "国产动漫", "香港剧", "韩国剧", "欧美剧", "国产剧"
+      ]
+    },
+    {
+      "key": "jszycj",
+      "name": "👖孟占朝丨极速丨采集",
+      "type": 1,
+      "api": "https://jszyapi.com/api.php/provide/vod/from/jsm3u8/at/json",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1,
+      "categories": [
+        "日剧", "马泰剧", "内地剧", "欧美剧", "香港剧", "韩剧", "台湾剧", "恐怖片", "动画片", "剧情片", "战争片", "动作片", "爱情片", "喜剧片", "科幻片", "纪录片", "悬疑片", "犯罪片", "中国动漫", "日本动漫", "欧美动漫", "大陆综艺", "短剧"
+      ]
+    }
+  ]
+}
